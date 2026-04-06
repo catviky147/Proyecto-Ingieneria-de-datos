a595ab303a3128c37205a0424ec80b3589e0c63f
greenvick147 <145700524+catviky147@users.noreply.github.com>
2026-04-06 07:29:54 -0500

    crear base de datos
    creo base de datos clinicaodontologica
     creo la tabla usuario con todos sus atributos en base a el diccionario de datos del proyecto

---
+++ baseDatosClinicaOdontologica.sql
@@ -0,0 +1,12 @@
+create database clinicaodontologica;
+use clinicaodontologica;
+create table usuario(
+idUsuario  int (10) primary key ,
+nombreUsuario varchar (50),
+telefono varchar (20),
+correoElectronico varchar (50),
+tipoDocumento varchar (10),
+userName varchar (20),
+contrasena varchar (16));
+
+
