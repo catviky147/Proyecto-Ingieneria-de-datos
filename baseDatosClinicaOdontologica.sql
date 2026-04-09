c842180eed81fcd481f16be72a6ea213ac3c254c
greenvick147 <145700524+catviky147@users.noreply.github.com>
2026-04-09 13:24:39 -0500

    implementacion base
    sigo implementando la base

--- baseDatosClinicaOdontologica.sql
+++ baseDatosClinicaOdontologica.sql
@@ -9,4 +9,28 @@ tipoDocumento varchar (10),
 userName varchar (20),
 contrasena varchar (16));
 
+create table odontologo(
+idOdontologo int auto_increment primary key,
+tarjetaProfesional varchar (50),
+especialidad varchar (50), 
+usuarioFK int (10),
+foreign key (usuarioFk) references usuario(idUsuario)
+);
+
+create table auxiliar(
+idOdontologo int auto_increment primary key,
+tarjetaProfesionalAux varchar (50), 
+usuarioFK int (10),
+foreign key (usuarioFk) references usuario(idUsuario)
+);
+
+create table citaOdontologico(
+idCity int auto_increment primary key,
+odontologo int not null, 
+paciente int not null,
+horario datetime,
+tratamiento int,
+foreign key (usuarioFk) references usuario(idUsuario)
+);
+
 
